3fd67abbfc7cd8f44c161de2a3633d17302ac12a
Victoria Delali <victoria.ake@unionsg.com>
2022-02-17 01:18:50 +0000

    Adding fonts and Assets folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My First Flutter App",
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset("assets/wildlife2.jpg"),
+                )),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.cyan,
+                child: Text("container 1"),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.green,
+                child: Text("container 2"),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.orange,
+                child: Text("container 3"),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click "),
+          backgroundColor: Colors.red[600],
+        ));
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:myapp/main.dart';
+import 'package:flutter_test/flutter_testmyapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
